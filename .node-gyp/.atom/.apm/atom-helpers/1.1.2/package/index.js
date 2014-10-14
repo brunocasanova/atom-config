24e0513483d3b7055b130840dde447779fabe2ce
José Moreira <jmoreira@microdual.com>
2014-10-14 19:07:01 +0100

    remove .node-gyp

--- .node-gyp/.atom/.apm/atom-helpers/1.1.2/package/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @name atom-helpers
- * @author Nicolas Tallefourtane <dev@nicolab.net>
- * @link https://github.com/Nicolab/atom-helpers
- * @license MIT https://github.com/Nicolab/atom-helpers/blob/master/LICENSE
- */
-
-module.exports.editor  = require('./lib/editor');
-module.exports.extends = require('./lib/utils').extends;
--- .node-gyp/.atom/.apm/git-show/0.0.3/package/spec/git-show-view-spec.coffee
+++
@@ -1,6 +0,0 @@
-AtomGitShowView = require '../lib/git-show-view'
-{WorkspaceView} = require 'atom'
-
-describe "Git ShowView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
